581c0de72224c1eef651f26623d46f69d471d0d1
Liz <lilita.moreno@hotmail.com>
2018-06-18 16:27:10 -0300

    update tarjetas.sql

--- tarjetas.sql
+++ tarjetas.sql
@@ -127,26 +127,26 @@ alter table alerta   add constraint alerta_fk0  foreign key (nrotarjeta)  refere
 --INSERCION DE DATOS
 
 --CLIENTES
-insert into cliente values(1,  'José','Argento','Godoy Cruz 1064','4584-3863');
-insert into cliente values(2,  'Mercedes', 'Benz', 'Pte Perón 1223','4665-8989');
-insert into cliente values(3,  'Megan', 'Ocaranza', 'Tribulato 2345', '4500-7651');
-insert into cliente values(4,  'Luis', 'Rios', 'Dorrego 1234', '4213-0153');
-insert into cliente values(5,  'Julio', 'Cortazar', ' Av Balbin 534', '4890-8747');
-insert into cliente values(6,  'Tomás', 'Aguirre', 'Urquiza 540', '4707-1600');
-insert into cliente values(7,  'Juan', 'Avalos', 'Av E Perón 7716', '4589-3191');
-insert into cliente values(8,  'Prudencia', 'Arzuaga','Rivadavia 416');
-insert into cliente values(9,  'Damiana', 'Molina', 'Malvinas 890', '4129-0964');
-insert into cliente values(10, 'Ramón', 'Perez', 'Las Heras 460', '4556-8970');
-insert into cliente values(11, 'Fernando', 'Álvarez', 'Tribulato 1290', '4908-7822');
-insert into cliente values(12, 'Carla', 'Estrella', 'Primera Junta 7865');
-insert into cliente values(13, 'Farid', 'Hasan', 'España 438', '4123-9078');
-insert into cliente values(14, 'Alicia', 'Castillo', 'Rodriguez Peña 170', '4367-7801');
-insert into cliente values(15, 'Elsa', 'López', 'Argüero 1138','4563-2323');
-insert into cliente values(16, 'Harry', 'Potter', 'Gutiérrez 908', '4768-9475');
-insert into cliente values(17, 'Pedro', 'Moreno', ' Julian Rejala 999',' 4556-9872');
-insert into cliente values(18, 'Ofelia', 'Le Brun', 'Paunero 7856','4389-7531');
-insert into cliente values(19, 'Fiona', 'Las Margaritas 484', '4895-7939');
-insert into cliente values(20, 'Evan', 'Peters', 'Irigoin 296',' 4664-1640');
+insert into cliente values(1,  'José',      'Argento',      'Godoy Cruz 1064',      '4584-3863');
+insert into cliente values(2,  'Mercedes',  'Benz',         'Pte Perón 1223',       '4665-8989');
+insert into cliente values(3,  'Megan',     'Ocaranza',     'Tribulato 2345',       '4500-7651');
+insert into cliente values(4,  'Luis',      'Rios',         'Dorrego 1234',         '4213-0153');
+insert into cliente values(5,  'Julio',     'Cortazar',     ' Av Balbin 534',       '4890-8747');
+insert into cliente values(6,  'Tomás',     'Aguirre',      'Urquiza 540',          '4707-1600');
+insert into cliente values(7,  'Juan',      'Avalos',       'Av E Perón 7716',      '4589-3191');
+insert into cliente values(8,  'Prudencia', 'Arzuaga',      'Rivadavia 416',        '4665-2333');
+insert into cliente values(9,  'Damiana',   'Molina',       'Malvinas 890',         '4129-0964');
+insert into cliente values(10, 'Ramón',     'Perez',        'Las Heras 460',        '4556-8970');
+insert into cliente values(11, 'Fernando',  'Álvarez',      'Tribulato 1290',       '4908-7822');
+insert into cliente values(12, 'Carla',     'Estrella',     'Primera Junta 7865',   '4444-5625');
+insert into cliente values(13, 'Farid',     'Hasan',        'España 438',           '4123-9078');
+insert into cliente values(14, 'Alicia',    'Castillo',     'Rodriguez Peña 170',   '4367-7801');
+insert into cliente values(15, 'Elsa',      'López',        'Argüero 1138',         '4563-2323');
+insert into cliente values(16, 'Harry',     'Potter',       'Gutiérrez 908',        '4768-9475');
+insert into cliente values(17, 'Pedro',     'Moreno',       'Julian Rejala 999',    '4556-9872');
+insert into cliente values(18, 'Ofelia',    'Le Brun',      'Paunero 7856',         '4389-7531');
+insert into cliente values(19, 'Laura',     'Martin',       'Las Margaritas 484',   '4895-7939');
+insert into cliente values(20, 'Evan',      'Peters',       'Irigoin 296',          '4664-1640');
 
 	--TARJETAS
 
