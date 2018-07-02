a122bf4ea81d2c109e41e7ce33fe6141c3e89249
Verónica Juarez <veronicajuarez01@hotmail.com>
2018-07-02 00:47:25 +0000

    Se agrego compra en tpJSON.go

--- tpJSON.go
+++ tpJSON.go
@@ -37,6 +37,15 @@ type Comercio struct {
 	Telefono     string
 }
 
+type Compra struct {
+    NroOperacion int
+    NroTarjeta   string
+    NroComercio  int
+    Fecha        string
+    Monto        int
+    Pagado       bool
+}
+
 var clientes = []Cliente{
 	{NroCliente: 1, Nombre: "Jose", Apellido: "Argento", Domicilio: "Godoy Cruz 1064", Telefono: "4584-3863"},
 	{NroCliente: 2, Nombre: "Mercedes", Apellido: "Benz", Domicilio: "Pte Peron 1223", Telefono: "4665-89892"},
@@ -52,6 +61,11 @@ var comercios = []Comercio{
 	{NroComercio: 1, Nombre: "Anubis", Domicilio: "Av. Pres. Juan Domingo Peron 3497", Codigopostal: "1613", Telefono: "4463-5343"},
 	{NroComercio: 2, Nombre: "Si A La Pizza", Domicilio: "25 de Mayo 2502", Codigopostal: "1613", Telefono: "4463-2314"},
 	{NroComercio: 3, Nombre: "Narrow", Domicilio: "Av. Pres. Juan Domingo Peron 1420", Codigopostal: "1663", Telefono: "4667-7297"},
+}
+var compras = []Compra{
+    {NroOperacion:1,NroTarjeta:"5703068016463339",NroComercio:3,Fecha:"2018-06-23",Monto:110,Pagado:true},
+    {NroOperacion:2,NroTarjeta:"5578153904072665",NroComercio:1,Fecha:"2018-04-03",Monto:60,Pagado:true},
+    {NroOperacion:3,NroTarjeta:"5578153904072665",NroComercio:2,Fecha:"2018-05-03",Monto:50,Pagado:true},
 }
 
 func CreateUpdate(db *bolt.DB, bucketName string, key []byte, val []byte) error {
@@ -183,6 +197,37 @@ func Comercios(db *bolt.DB) {
 
 }
 
+func Compras(db *bolt.DB) {
+
+	nro := Compra{1, "5703068016463339", 3, "2018-06-23", 110, true}
+	data, err := json.Marshal(nro)
+	if err != nil {
+		log.Fatal(err)
+	}
+	CreateUpdate(db, "compras", []byte(strconv.Itoa(nro.NroOperacion)), data)
+	resultado, err := ReadUnique(db, "compras", []byte(strconv.Itoa(nro.NroOperacion)))
+	fmt.Printf("%s\n", resultado)
+
+	nro2 := Compra{2, "5578153904072665", 1, "2018-04-03", 60, true}
+	data2, err := json.Marshal(nro2)
+	if err != nil {
+		log.Fatal(err)
+	}
+	CreateUpdate(db, "compras", []byte(strconv.Itoa(nro2.NroOperacion)), data2)
+	resul, err := ReadUnique(db, "compras", []byte(strconv.Itoa(nro2.NroOperacion)))
+	fmt.Printf("%s\n", resul)
+
+	nro3 := Compra{3, "5578153904072665", 2, "2018-05-03", 50,true}
+	data3, err := json.Marshal(nro3)
+	if err != nil {
+		log.Fatal(err)
+	}
+	CreateUpdate(db, "compras", []byte(strconv.Itoa(nro3.NroOperacion)), data3)
+	resul3, err := ReadUnique(db, "compras", []byte(strconv.Itoa(nro3.NroOperacion)))
+	fmt.Printf("%s\n", resul3)
+
+}
+
 func main() {
 
 	db, err := bolt.Open("postgres.db", 0600, nil)
@@ -195,6 +240,7 @@ func main() {
 	Clientes(db)
 	Tarjetas(db)
 	Comercios(db)
+	Compras(db)
 	//	LeerDatosUsuario(db);
 	/*
 		data, err := json.MarshalIndent(clientes, "", "    ")
